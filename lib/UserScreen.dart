79cfc604c0be0a3c5df86fa3f0b48e7f11364447
subhammahanty235 <subhammahanty235@gmail.com>
2023-03-27 21:37:55 +0530

    Log out button added

--- lib/UserScreen.dart
+++ lib/UserScreen.dart
@@ -1,14 +1,22 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, must_be_immutable
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, must_be_immutable, use_build_context_synchronously
 
-
+import 'package:basic1/LoginScreen.dart';
+import 'package:basic1/SignUpScreen.dart';
 import 'package:basic1/dataBaseHelpers/mongodb.dart';
 import 'package:basic1/main.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-
-class UserScreen extends StatelessWidget {
+class UserScreen extends StatefulWidget {
   var userId; //this is to be sent to the mongodb to get the user's details
   UserScreen({super.key, this.userId});
+  @override
+
+  State<UserScreen> createState() => _UserScreenState();
+}  
+
+class _UserScreenState extends State<UserScreen>{
+  
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +47,7 @@ class UserScreen extends StatelessWidget {
             )),
         body: SafeArea(
             child: FutureBuilder(
-          future: MongoDatabase.getUserData(userId),
+          future: MongoDatabase.getUserData(widget.userId),
           builder: (context, AsyncSnapshot snapshot) {
             // print("---------------------------calling------------------------");
             // print(snapshot.data['profiledetails']['name']);
@@ -50,7 +58,7 @@ class UserScreen extends StatelessWidget {
             } else {
               if (snapshot.hasData) {
                 // print("--------------------------------has data called-----------------");
-                
+
                 return Column(
                   children: [
                     Container(
@@ -69,14 +77,15 @@ class UserScreen extends StatelessWidget {
                             borderRadius: BorderRadius.circular(10)),
                         child: Row(
                           children: [
-                            CircleAvatar(child: Icon(Icons.person )),
+                            CircleAvatar(child: Icon(Icons.person)),
                             Padding(
                                 padding: EdgeInsets.symmetric(
                                     vertical: 10, horizontal: 10),
                                 child: Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    Text(snapshot.data['profiledetails']['name'],
+                                    Text(
+                                        snapshot.data['profiledetails']['name'],
                                         style: TextStyle(
                                             fontSize: 16,
                                             fontWeight: FontWeight.w400)),
@@ -107,7 +116,8 @@ class UserScreen extends StatelessWidget {
                                     children: [
                                       Icon(Icons.sports_baseball_rounded,
                                           color: Colors.blue, size: 20),
-                                      Text(" Name : ${snapshot.data['profiledetails']['name']}",
+                                      Text(
+                                          " Name : ${snapshot.data['profiledetails']['name']}",
                                           style: TextStyle(
                                             fontSize: 18,
                                           ))
@@ -123,7 +133,8 @@ class UserScreen extends StatelessWidget {
                                     children: [
                                       Icon(Icons.sports_baseball_rounded,
                                           color: Colors.blue, size: 20),
-                                      Text(" Age : ${snapshot.data['profiledetails']['age']}",
+                                      Text(
+                                          " Age : ${snapshot.data['profiledetails']['age']}",
                                           style: TextStyle(
                                             fontSize: 18,
                                           ))
@@ -139,7 +150,8 @@ class UserScreen extends StatelessWidget {
                                     children: [
                                       Icon(Icons.sports_baseball_rounded,
                                           color: Colors.blue, size: 20),
-                                      Text(" Gender : ${snapshot.data['profiledetails']['gender']}",
+                                      Text(
+                                          " Gender : ${snapshot.data['profiledetails']['gender']}",
                                           style: TextStyle(
                                             fontSize: 18,
                                           ))
@@ -155,7 +167,8 @@ class UserScreen extends StatelessWidget {
                                     children: [
                                       Icon(Icons.sports_baseball_rounded,
                                           color: Colors.blue, size: 20),
-                                      Text(" Country : ${snapshot.data['profiledetails']['country']}",
+                                      Text(
+                                          " Country : ${snapshot.data['profiledetails']['country']}",
                                           style: TextStyle(
                                             fontSize: 18,
                                           ))
@@ -171,7 +184,8 @@ class UserScreen extends StatelessWidget {
                                     children: [
                                       Icon(Icons.sports_baseball_rounded,
                                           color: Colors.blue, size: 20),
-                                      Text(" Hobbies : ${snapshot.data['profiledetails']['hobbies']}",
+                                      Text(
+                                          " Hobbies : ${snapshot.data['profiledetails']['hobbies']}",
                                           style: TextStyle(
                                             fontSize: 18,
                                           ))
@@ -181,9 +195,21 @@ class UserScreen extends StatelessWidget {
                                 //  Divider(height: 5,),
                                 // Padding(padding:EdgeInsets.all(10),  child: Row(children:[Icon(Icons.sports_baseball_rounded , color: Colors.blue , size:20) , Text("Name : Subham Mahanty" ,style:TextStyle(fontSize: 18 , ) )],) ,),
                               ],
-                            )
+                            ),
                           ],
-                        ))
+                        )),
+                    Container(
+                     width: MediaQuery.of(context).size.width - 40,
+                     decoration: BoxDecoration(
+                      border: Border.all(color: Colors.red.shade500),
+                      borderRadius: BorderRadius.circular(10)
+                      
+                     ),
+                        child: TextButton(
+                            style:TextButton.styleFrom(
+                                foregroundColor: Colors.red
+                            ),
+                            onPressed: () {_logout();}, child: Text("Log Out" , style: TextStyle(color: Colors.red[600]),)))
                   ],
                 );
               } else {
@@ -198,5 +224,18 @@ class UserScreen extends StatelessWidget {
             }
           },
         )));
+  }
+
+  void _logout ()async{
+    final pref = await SharedPreferences.getInstance();
+
+    pref.remove("ID_USER");
+    pref.remove("NAME_USER");
+    pref.setBool("USER_LOGGEDIN", false);
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Logged Out"), backgroundColor: Colors.red[300],));
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginScreen()));
+    
+
+
   }
 }
